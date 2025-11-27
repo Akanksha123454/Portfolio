bbdcdd63acb0a7791b03727097c555a4cead8e80
akanksha <akankshathombare9@gmail.com>
2025-11-27 15:51:04 +0530

    new change

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -12,7 +12,7 @@ import "./App.css";
 function App() {
   return (
     <div className="app-wrapper">
-      <BrowserRouter>
+      <BrowserRouter basename="/Portfolio">
         <Navbar />
 
         <div className="content">
